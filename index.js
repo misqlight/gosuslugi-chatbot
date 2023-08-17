0b5af36cedc9943d0eb9f420133fb93e88cbe0e5
misqlight <vimefeed@gmail.com>
2023-08-17 08:07:39 +0300

    Random session ID generator

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const http = require('https');
 
+function generateString(length, symbols = '0123456789abcdef') {
+    let result = '';
+    for (let i = 0; i < length; i++)
+        result += symbols[Math.floor(Math.random() * symbols.length)];
+    return result;
+}
+
 class Chatbot {
     token;
 
     async _init(sessionId, platform = "epgu_desc") {
+        if (!/^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$/.test(sessionId))
+            sessionId = [generateString(8), generateString(4), '4' + generateString(3), generateString(1, '89ab') + generateString(3), generateString(12)].join('-');
+
         return new Promise((resolve, reject) => {
             const request = http.request({
                 method: 'POST',
